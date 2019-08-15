8bc27556bc6b8c8f0078054a61d86db0f36fd006
Filipe Silva <filipematossilva@gmail.com>
2019-08-15 17:37:13 +0100

    actually do v7

--- benchmark-project.sh
+++ benchmark-project.sh
@@ -63,7 +63,7 @@ sed -i s/\"target\"\:\ \"es2015\"/\"target\"\:\ \"es5\"/g tsconfig.json
 $benchmark_command
 
 echo -e "\n# Install CLI 7\n"
-silent "$install_cli_8"
+silent "$install_cli_7"
 npm run ng version
 
 echo -e "\n# Benchmark CLI version 7\n"
