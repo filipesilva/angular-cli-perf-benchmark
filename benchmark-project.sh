ce4fe2d3a9bdd4118635cf19e5ba75687335119c
Filipe Silva <filipematossilva@gmail.com>
2019-08-15 18:18:13 +0100

    few fixes to the benchmark script

--- benchmark-project.sh
+++ benchmark-project.sh
@@ -14,7 +14,7 @@ fi
 install_fw="$install_package_command -S @angular/animations@8.2.2 @angular/common@8.2.2 @angular/core@8.2.2 @angular/elements@8.2.2 @angular/forms@8.2.2 @angular/platform-browser@8.2.2 @angular/platform-browser-dynamic@8.2.2 @angular/router@8.2.2 @angular/service-worker@8.2.2"
 install_fw_dev="$install_package_command -D @angular/compiler@8.2.2 @angular/compiler-cli@8.2.2 @angular/language-service@8.2.2 typescript@3.5.3"
 install_cli_7="$install_package_command -D @angular/cli@7.3.9 @angular-devkit/build-angular@0.13.9"
-install_cli_8="$install_package_command -D @angular/cli@8.3.0-rc.0 @angular-devkit/build-angular@0.803.0-rc.0 node-sass"
+install_cli_8="$install_package_command -D @angular/cli@8.3.0-rc.0 @angular-devkit/build-angular@0.803.0-rc.0 node-sass@4.12.0"
 
 # Set to true to debug.
 DEBUG=false
@@ -50,15 +50,21 @@ echo -e "\n# Install FW 8\n"
 silent "$install_fw"
 silent "$install_fw_dev"
 
-echo -e "\n# CLI version 8 with differential loading\n"
+echo -e "\n# Install CLI 8\n"
 silent "$install_cli_8"
+npm run ng version
+
+echo -e "\n# Benchmark CLI version 8 with differential loading\n"
 $benchmark_command
 
-echo -e "\n# CLI version 8 without differential loading\n"
+echo -e "\n# Benchmark CLI version 8 without differential loading\n"
 # Didn't set this one as silent because it's already silent and double escaping is hell.
 sed -i s/\"target\"\:\ \"es2015\"/\"target\"\:\ \"es5\"/g tsconfig.json
 $benchmark_command
 
-echo -e "\n# CLI version 7\n"
-silent "$install_cli_7"
-$benchmark_command
+# echo -e "\n# Install CLI 7\n"
+# silent "$install_cli_7"
+# npm run ng version
+
+# echo -e "\n# Benchmark CLI version 7\n"
+# $benchmark_command
