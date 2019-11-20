514030cb7d0123362e16e2bf77e15f3d5f7ee9fd
Alan Agius <alan.agius4@gmail.com>
2019-11-20 11:29:20 +0100

    fix cli version

--- global-setup.sh
+++ global-setup.sh
@@ -5,7 +5,7 @@ echo -e "# Set global npm install directory"
 echo "export PATH=~/.npm-global/bin:$PATH" >> ~/.profile
 source ~/.profile
 echo -e "\n# Install global Angular CLI and Benchmark utility"
-npm i -g @angular/cli@9.0.0-rc2
+npm i -g @angular/cli@9.0.0-rc.2
 npm i -g ./angular-devkit-benchmark-0.800.0-beta.18.tgz
 echo -e "\n# Print npm version"
 npm --version
