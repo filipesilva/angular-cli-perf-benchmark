4559f583c78d768931e0b4adedb57af4744974a3
Alan Agius <alan.agius4@gmail.com>
2020-01-24 14:38:58 +0100

    aio

--- watch_script.js
+++ watch_script.js
@@ -1,4 +1,4 @@
 const fs = require('fs')
-const file = 'projects/one/src/app/a4/a1/a11/a11.component.ts';
+const file = 'aio/src/app/shared/copier.service.ts';
 const data = fs.readFileSync(file, 'utf8');
-fs.writeFileSync(file, data.replace('this.fibonacci(10);', 'this.fibonacci(10);console.log(true);'));
+fs.writeFileSync(file, data.replace('9999', '99999'));
