553fb32da39405e4f5b0d75078279d2cf8c225cd
Alan Agius <alan.agius4@gmail.com>
2020-01-24 15:19:20 +0100

    test

--- watch_script.js
+++ watch_script.js
@@ -1,4 +1,7 @@
 const fs = require('fs')
 const file = 'aio/src/app/shared/copier.service.ts';
 const data = fs.readFileSync(file, 'utf8');
-fs.writeFileSync(file, data.replace('9999', '99999'));
+setTimeout(() => fs.writeFileSync(file, data.replace('9999', '99999')), 1000)
+
+
+
